e944770c30e54ad740aa66482a5eba9ce66f5722
bambademe <cheikhbambademe@gmail.com>
2022-01-25 12:19:31 -0800

    interface model, task ans todos props...

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import "./style.css";
+import React, { useRef } from 'react';
+import "./inputStyle.css";
 
-const InputField = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+const InputField = ({todo,setTodo, handleAdd}: Props) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
   return (
-      <form className='input'>
-          <input type="input" className='input__box' placeholder='Enter a task'/>
+      <form className='input' onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}>
+          <input 
+            ref={inputRef}
+            type="input" 
+            className='input__box' 
+            placeholder='Enter a task'
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            />
           <button type='submit' className='input__submit'>Go</button>
       </form>
   );
